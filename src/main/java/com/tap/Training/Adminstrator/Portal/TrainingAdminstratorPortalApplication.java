8077992db41d56e129ec926a8c7bdb3bc3a1ddc5
Ashish Singh <ashish1qwerty1@gmail.com>
2025-08-12 16:46:52 +0530

    Initial commit

---
+++ src/main/java/com/tap/Training/Adminstrator/Portal/TrainingAdminstratorPortalApplication.java
@@ -0,0 +1,13 @@
+package com.tap.Training.Adminstrator.Portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAdminstratorPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingAdminstratorPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tap/Training/Adminstrator/Portal/TrainingAdminstratorPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap.Training.Adminstrator.Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAdminstratorPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
